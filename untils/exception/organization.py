4d946048c559098617d7302cee9d7d8784e180a8
Pythoner-Umka <112894460+DimaChepuchtanov@users.noreply.github.com>
2025-11-07 20:33:36 +0500

    Delete untils/exception directory

--- untils/exception/organization.py
+++
@@ -1,6 +0,0 @@
-class OrganizationNotFound(Exception):
-    def __init__(self, param):
-        self.param = param
-
-    def __str__(self):
-        return f"Организация со значением '{self.param}' не найдена"
